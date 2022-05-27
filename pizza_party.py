7febcaf55aaf05b2bedb0486b6de8427607b6305
superantimony <101242938+superantimony@users.noreply.github.com>
2022-05-27 14:24:10 +0800

    Add files via upload

---
+++ pizza_party.py
@@ -0,0 +1,8 @@
+number=64
+n=1
+p=2
+while p<number:
+        p=(n*n+n+2)/2
+        print("the number of cuts is",n,"the number of pieces of pizza",p)
+        n+=1
+
---
+++ triangle.py
@@ -0,0 +1,7 @@
+number=10
+sum=0
+counter=1
+while counter<=10:
+        sum=sum+counter
+        counter+=1
+print("sum =", sum)
