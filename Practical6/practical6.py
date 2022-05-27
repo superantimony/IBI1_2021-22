c6156992de1374d68e413f26af9268c37bc66b6e
superantimony <101242938+superantimony@users.noreply.github.com>
2022-05-27 14:30:01 +0800

    Add files via upload

---
+++ Practical6/practical6.py
@@ -0,0 +1,39 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings
+import numpy as np
+import matplotlib.pyplot as plt
+N=10
+paternal_age=[30,35,40,45,50,55,60,65,70,75]
+chd=[1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94]
+plt.scatter(paternal_age,chd,marker='o')
+plt.show()
+
+import numpy as np
+import matplotlib.pyplot as plt
+n=8
+marks=[45,36,86,57,53,92,65,45]
+plt.boxplot(score,
+            vert = True,
+            whis = 1.5,
+            patch_artist = True,
+            meanline = False,
+            showbox = True,
+            showcaps = True,
+            showfliers = True,
+            notch = False,
+                )
+plt.show()
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
