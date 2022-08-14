27072f4e2c0119dace9ef8ddb7976366fea4a4da
superantimony <101242938+superantimony@users.noreply.github.com>
2022-08-15 01:13:29 +0800

    variables

---
+++ Practical5/variables.py
@@ -0,0 +1,12 @@
+a=806966
+b=518547
+c=672391
+d=a-c
+e=c-b
+print("d=", d)
+print("e=", e)
+if d>e:
+    print("d is bigger than e, the population of Washington, DC closer to Haining")
+else:
+    print("d is smaller than e, the population of Washington, DC closer to Edinburgh")
+#answer: d is smaller than e, the population of Washington, DC closer to Edinburgh
